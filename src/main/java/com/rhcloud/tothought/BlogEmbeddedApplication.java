45e2768ac96a090153d622acfe4b9d7eb504b5e8
Kevin Bowersox <kmb385@gmail.com>
2017-09-23 15:03:21 -0400

    Updated

--- src/main/java/com/rhcloud/tothought/BlogEmbeddedApplication.java
+++ src/main/java/com/rhcloud/tothought/BlogEmbeddedApplication.java
@@ -8,33 +8,39 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.SecurityFilterAutoConfiguration;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
-import org.springframework.web.servlet.DispatcherServlet;
 
 @SpringBootApplication
 @ImportResource({ "classpath:/META-INF/application-context.xml", "classpath:/META-INF/servlet-context.xml" })
 @EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class, SecurityFilterAutoConfiguration.class,
 		SecurityAutoConfiguration.class })
 @Configuration
-public class BlogEmbeddedApplication {
+public class BlogEmbeddedApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogEmbeddedApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BlogEmbeddedApplication.class);
 	}
 
 	@Bean
 	public Filter openSessionInView() {
 		return new OpenEntityManagerInViewFilter();
 	}
-
+/*
 	@Bean
 	public ServletRegistrationBean dispatcherRegistration(DispatcherServlet dispatcherServlet) {
 		ServletRegistrationBean registration = new ServletRegistrationBean(dispatcherServlet);
 		registration.addUrlMappings("/*");
 		return registration;
 	}
+	*/
 }

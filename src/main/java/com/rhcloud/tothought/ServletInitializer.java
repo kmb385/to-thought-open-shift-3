7f6d16a7528f1f2d91f388eef9fd3f48c5a7d912
Kevin Bowersox <kmb385@gmail.com>
2017-09-23 09:41:50 -0400

    Updated

--- src/main/java/com/rhcloud/tothought/ServletInitializer.java
+++ src/main/java/com/rhcloud/tothought/ServletInitializer.java
@@ -1,12 +1,27 @@
 package com.rhcloud.tothought;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class ServletInitializer extends SpringBootServletInitializer {
-/*
+
+	
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		ServletRegistration.Dynamic registration = servletContext.addServlet("dispatcher", new DispatcherServlet());
+		registration.setLoadOnStartup(1);
+		registration.addMapping("/");
+		super.onStartup(servletContext);
+	}
+
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(BlogEmbeddedApplication.class);
 	}
-*/
+
 }

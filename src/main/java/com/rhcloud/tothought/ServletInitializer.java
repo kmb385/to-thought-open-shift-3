adf18e994685d1fcdb255e116a3bea8eeaf560cb
Kevin Bowersox <kmb385@gmail.com>
2017-09-23 09:03:49 -0400

    Updated

--- src/main/java/com/rhcloud/tothought/ServletInitializer.java
+++ src/main/java/com/rhcloud/tothought/ServletInitializer.java
@@ -1,6 +1,5 @@
 package com.rhcloud.tothought;
 
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {

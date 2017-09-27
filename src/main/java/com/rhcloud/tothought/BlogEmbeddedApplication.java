068b926bd1d8abef9c2a0941a9b1a73d940e003b
Kevin Bowersox <kmb385@gmail.com>
2017-09-27 14:35:08 -0400

    Updated

--- src/main/java/com/rhcloud/tothought/BlogEmbeddedApplication.java
+++ src/main/java/com/rhcloud/tothought/BlogEmbeddedApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.SecurityFilterAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
